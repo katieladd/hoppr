40c8a37a9f9a5c7f55966aa2e9e0681d1a9d2a11
Katie <katieqgriffith@gmail.com>
2021-12-06 23:26:08 -0500

    Implemented Map.tsx in Brewery Details, to render a map

---
+++ brewery-app/src/components/BreweryDetails.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Brewery } from "./App"
+import Map from "./Map"
+
+const BreweryDetails = () => {
+  return (
+    <div>
+      <Map/>
+    </div>
+  )
+}
+
+export default BreweryDetails;
